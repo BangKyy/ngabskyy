87e0648e888b3ef28eabc5dfc838be919966977b
unknown <xenemies.27@gmail.com>
2024-11-14 18:15:36 +0700

    Updating to v2.5.8 is adding features and updating portfolio data, namely Posters, Logos, Others

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,7 +1,11 @@
-import Work1Design from "../../assets/design1.png";
-import Work2Design from "../../assets/design2.png";
-import Work3Design from "../../assets/design3.png";
-import Work4Design from "../../assets/design4.png";
+import Work1UIX from "../../assets/UIX1.png";
+import Work2UIX from "../../assets/UIX2.png";
+import Work3UIX from "../../assets/UIX3.png";
+import Work1Logo from "../../assets/design1.png";
+import Work1Poster from "../../assets/design2.png";
+import Work2Poster from "../../assets/design3.jpg";
+import Work3Poster from "../../assets/design4.png";
+import Work4Poster from "../../assets/design5.jpg";
 import Work1Web from "../../assets/web3.png";
 import Work2Web from "../../assets/web4.png";
 import Work3Web from "../../assets/web5.png";
@@ -17,103 +21,136 @@ import Work7Certificate from "../../assets/sertifikat2.jpg";
 export const projectsData = [
     {
       id: 1,
-      image: Work1Design,
+      image: Work1UIX,
       title: "UI Class Website",
-      category: "design",
+      category: "ui/ux",
       link: "https://www.figma.com/design/hrVzwsDmtqb8Db8G2yLkl6/PROJECT-WEB-CLASS?node-id=0-1&t=7B9UHsO1SCzSbMVT-1"
     },
     {
       id: 2,
-      image: Work2Design,
+      image: Work2UIX,
       title: "UI/UX PT Rumah Mesin Kreatif",
-      category: "design",
+      category: "ui/ux",
       link: "https://www.behance.net/gallery/200107291/Heavy-Equipment-Company-UIUX-Design-Website"
     },
     {
       id: 3,
-      image: Work3Design,
+      image: Work3UIX,
       title: "UI/UX AFC ASIAN CUP APP",
-      category: "design",
+      category: "ui/ux",
       link: "https://www.behance.net/gallery/200099901/AFC-Asian-Cup-App-UIUX-Design-Portfolio"
     },
     {
       id: 4,
-      image: Work4Design,
+      image: Work1Logo,
       title: "Personal Logo Design",
-      category: "design",
+      category: "logo",
       link: "https://www.instagram.com/p/DAwJvWyhmCr/?igsh=bXd5cnl5dXJ2N2w="
+      // link: "https://drive.google.com/file/d/1-s1SkKuRC_6f04pP5czinHEMNLV_1r9g/view?usp=sharing"
     },
     {
       id: 5,
+      image: Work1Poster,
+      title: "Class Documentary Film Poster Design",
+      category: "poster",
+      link: "https://drive.google.com/file/d/1b_qSud6eXRveLY1zU0smCvI6aeq4ZQOP/view?usp=drive_link"
+      // link: "https://www.instagram.com/p/DAwJvWyhmCr/?igsh=bXd5cnl5dXJ2N2w="
+    },
+    {
+      id: 6,
+      image: Work2Poster,
+      title: "Cat Poster Design",
+      category: "poster",
+      link : "https://drive.google.com/file/d/17i1Ewk1SEtzgWXv5EWVoe0uJgRifqQKV/view?usp=drive_link"
+      // link: "https://www.instagram.com/p/DAwJvWyhmCr/?igsh=bXd5cnl5dXJ2N2w="
+    },
+    {
+      id: 7,
+      image: Work3Poster,
+      title: "Cyber ​​Bullying Poster Design",
+      category: "poster",
+      link: "https://drive.google.com/file/d/1wwooykZl50bFyfnJO8veCiklTWkU3vwN/view?usp=drive_link"
+      // link: "https://www.instagram.com/p/DAwJvWyhmCr/?igsh=bXd5cnl5dXJ2N2w="
+    },
+    {
+      id: 8,
+      image: Work4Poster,
+      title: "Redesign of Indomilk Brand Poster",
+      category: "poster",
+      link: "https://drive.google.com/file/d/1KUosLNa4L_I2t8TwvOqUZ97nWO1uH4Nv/view?usp=drive_link"
+      // link: "https://www.instagram.com/p/DAwJvWyhmCr/?igsh=bXd5cnl5dXJ2N2w="
+    },
+    {
+      id: 9,
       image: Work1Web,
       title: "Movies Information Website",
       category: "website",
       link: "https://kyy-movies.netlify.app/"
     },
     {
-      id: 6,
+      id: 10,
       image: Work2Web,
       title: "PMR Event Website",
       category: "website",
       link: "https://baratamadyaxi-smanta.netlify.app/"
     },
     {
-      id: 7,
+      id: 11,
       image: Work3Web,
       title: "SMAN Science Six Class Website",
       category: "website",
       link: "https://sman1talun.sch.id/sciesixx/"
     },
     {
-      id: 8,
+      id: 12,
       image: Work4Web,
       title: "PT Rumah Mesin Kreatif Website",
       category: "website",
       link: "https://rumek.netlify.app"
     },
     {
-      id: 9,
+      id: 13,
       image: Work1Certificate,
       title: "Belajar Dasar Pemrograman Web",
       category: "certificate",
       link: "https://www.dicoding.com/certificates/QLZ9775KEP5D"
     },
     {
-      id: 10,
+      id: 14,
       image: Work2Certificate,
       title: "Python Fundamental",
       category: "certificate",
       link: "https://www.dicoding.com/certificates/QLZ97GY67P5D"
     },
     {
-      id: 11,
+      id: 15,
       image: Work3Certificate,
       title: "AI Fundamental",
       category: "certificate",
       link: "https://www.dicoding.com/certificates/MEPJNN444X3V"
     },
     {
-      id: 12,
+      id: 16,
       image: Work4Certificate,
       title: "Data Visualization Fundamental",
       category: "certificate",
       link: "https://www.dicoding.com/certificates/JLX17J3Q5X72"
     },
     {
-      id: 13,
+      id: 17,
       image: Work5Certificate,
       title: "SQL Fundamental",
       category: "certificate",
       link: "https://www.dicoding.com/certificates/JLX17J7EGX72"
     },
     {
-      id: 14,
+      id: 18,
       image: Work6Certificate,
       title: "Website Development",
       category: "certificate"
     },
     {
-      id: 15,
+      id: 19,
       image: Work7Certificate,
       title: "Python Course",
       category: "certificate"
@@ -131,7 +168,16 @@ export const projectsNav = [
         name: 'certificate',
     },
     {
-        name: 'design',
+        name: 'ui/ux',
+    },
+    {
+        name: 'poster',
+    },
+    {
+        name: 'logo',
+    },
+    {
+        name: 'others',
     },
 ];
   
